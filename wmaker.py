3391690b0634a514bb9d462677d38185a3e58640
Amin Solhi <amin.solhi@gmail.com>
2025-04-11 15:12:22 +0330

    Add files via upload
    # Hex Number Generator
    
    ### Developer Information
    - **Name:** Amin Solhi
    - **Email:** [amin.solhi@gmail.com](mailto:amin.solhi@gmail.com)
    - **Phone:** +9891111842779
    
    This program generates a list of hexadecimal numbers based on a provided key and bit range defined by the user. It uses the SHA-256 hashing algorithm to create a seed for random number generation, ensuring that the output is consistent for the same key.
    
    ## Features
    
    - Generates random hexadecimal numbers.
    - Outputs the index, hexadecimal value, and its start and end range based on bit length.
    - Uses SHA-256 as a hashing function for deterministic random number generation.
    
    ## Requirements
    
    To run this program, you need:
    - Python 3.x installed on your machine.
    
    ## Usage
    
    1. Clone or download this repository.
    2. Open a terminal and navigate to the directory where the script is located.
    3. Run the program using the command:
    
       python wmakerpy
    
    4. Follow the prompts:
       - Enter the start bit range (as an integer).
       - Enter the end bit range (as an integer).
       - Enter a phrase as the key for randomization.
    
    ### Example
    
    ```plaintext
    Enter Start BitRange : 5
    Enter End BitRange : 10
    Enter Phrase pass : satoshi
    ```
    
    The output will display the index, hexadecimal value, and the corresponding start and end range.
    
    ## Output Format
    
    ```
    Index    Hex Value               Start Range          End Range
    -----------------------------------------------------------
    1        abcdef1234567890       20                   40
    2        fedcba0987654321       40                   80

---
+++ wmaker.py
@@ -0,0 +1,37 @@
+import hashlib  
+import random  
+
+
+def generate_hex_numbers(key, count):  
+    hash_input = key.encode('utf-8')  
+    hashed_value = hashlib.sha256(hash_input).hexdigest()  
+
+    hex_numbers = set()  
+    random.seed(hashed_value)  
+
+    index_width = 7  
+    hex_width = 25  
+    start_range_width = 30  
+    end_range_width = 30  
+
+  
+    print(f"{'Index':<{index_width}} {'Hex Value':<{hex_width}} {'Start Range':<{start_range_width}} {'End Range':<{end_range_width}}")  
+    print("-" * (index_width + hex_width + start_range_width + end_range_width + 4))  
+
+    for i in range(scount, count + 1):  
+        random_number = random.randint(2 ** i, 2 ** (i + 1))  
+        hex_number = hex(random_number).removeprefix('0x')  
+        start_range = f'{2 ** i:x}'  
+        end_range = f'{2 ** (i + 1):x}'  
+
+     
+        print(f"{i + 1:<{index_width}} {hex_number:<{hex_width}} {start_range:<{start_range_width}} {end_range:<{end_range_width}}")  
+
+    return  
+
+
+key = 'satoshi'  
+scount = int(input ("Enter Start BitRange : "))
+count = int(input ("Enter End BitRange : "))
+key = input ("Enter Phrase pass : ")
+hex_values = generate_hex_numbers(key, count)
